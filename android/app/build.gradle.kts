7a14edbc46b925af08e64f25309248875c0ea22c
yimoyimoyi <3255961367@qq.com>
2026-06-07 14:46:03 +0800

    chore: 独立版本 v0.9.102 custom，签名已配置

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -74,8 +74,8 @@ android {
         applicationId = packageName
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 10010010
-        versionName = "0.9.101 mode"
+        versionCode = 10010020
+        versionName = "0.9.102 custom"
         ndk {
             abiFilters.addAll(arrayOf("armeabi-v7a", "arm64-v8a", "x86_64"))
         }
